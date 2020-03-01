84c1263fe976f8fc3d12de2c630aac59934300a5
doncodie <don.codie@gmail.com>
2020-03-01 01:55:48 +0100

    [homepage] :   - header   - infos   - affiliate   - aside

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('pages/homepage/homepage.html.twig');
+    }
+}
