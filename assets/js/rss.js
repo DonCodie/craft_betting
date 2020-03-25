bc3be241a4768385e7830e74fa330ca630965730
doncodie <don.codie@gmail.com>
2020-03-26 00:05:56 +0100

    [homepage] : vip block   - vip block   - scss button + colors + borders

--- assets/js/rss.js
+++ assets/js/rss.js
@@ -1,10 +1,10 @@
 require ('../js/slick/slick.min');
 
 $(document).ready(function () {
-    $('.rss').slick({
+    $('.rss__sport-slider').slick({
         infinite: true,
-        slidesToShow: 7,
-        slidesToScroll: 7,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         nextArrow: '<div class="rss__arrow-div"><button class="rss__arrow rss__arrow-next"></button></div>',
         prevArrow: '<div class="rss__arrow-div"><button class="rss__arrow rss__arrow-previous"></button></div>'
     });
