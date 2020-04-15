798feb3d3c6a9c502301da8b2a73516920225ddb
doncodie <don.codie@gmail.com>
2020-04-15 13:57:26 +0200

    [admin] : style   - add icon and organize backend

---
+++ src/Migrations/Version20200415100635.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200415100635 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE prono ADD sport_id INT NOT NULL');
+        $this->addSql('ALTER TABLE prono ADD CONSTRAINT FK_9B3EC938AC78BCF8 FOREIGN KEY (sport_id) REFERENCES sport (id)');
+        $this->addSql('CREATE INDEX IDX_9B3EC938AC78BCF8 ON prono (sport_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE prono DROP FOREIGN KEY FK_9B3EC938AC78BCF8');
+        $this->addSql('DROP INDEX IDX_9B3EC938AC78BCF8 ON prono');
+        $this->addSql('ALTER TABLE prono DROP sport_id');
+    }
+}
