72766ecfe70d4e05e5c023924ed8ae25c6ddf0f8
doncodie <don.codie@gmail.com>
2020-04-21 18:23:12 +0200

    [hppronos] : responsive scss   - mobile   - tablet

--- assets/js/rss.js
+++ assets/js/rss.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 3,
+                    slidesToScroll: 3
                 }
             },
             {
@@ -25,45 +25,36 @@ $(document).ready(function () {
         ]
     });
 
+    // Class active sur les sports et remplacement du slider
     $('.rss__sport-subtitle-foot').click(function () {
         // Class active
         $(this).parent('.rss__sport-subtitles').find('.rss__sport-subtitle-active')
             .removeClass('rss__sport-subtitle-active')
         $(this).addClass('rss__sport-subtitle-active');
-        //
-        $('.rss__sport-foot').css('display', 'block');
-        $('.rss__sport-basket').css('display', 'none');
-        $('.rss__sport-tennis').css('display', 'none');
+        // Replace Slider
+        $('.rss__sport-slider-foot').css('visibility', 'visible');
+        $('.rss__sport-slider-basket').css('visibility', 'hidden');
+        $('.rss__sport-slider-tennis').css('visibility', 'hidden');
     });
     $('.rss__sport-subtitle-basket').click(function () {
         // Class active
         $(this).parent('.rss__sport-subtitles').find('.rss__sport-subtitle-active')
             .removeClass('rss__sport-subtitle-active')
         $(this).addClass('rss__sport-subtitle-active');
-        //
-        $('.rss__sport-basket').css('display', 'block');
-        $('.rss__sport-foot').css('display', 'none');
-        $('.rss__sport-tennis').css('display', 'none');
+        // Replace Slider
+        $('.rss__sport-slider-basket').css('visibility', 'visible');
+        $('.rss__sport-slider-foot').css('visibility', 'hidden');
+        $('.rss__sport-slider-tennis').css('visibility', 'hidden');
     });
     $('.rss__sport-subtitle-tennis').click(function () {
         // Class active
         $(this).parent('.rss__sport-subtitles').find('.rss__sport-subtitle-active')
             .removeClass('rss__sport-subtitle-active')
         $(this).addClass('rss__sport-subtitle-active');
-        //
-        $('.rss__sport-tennis').css('display', 'block');
-        $('.rss__sport-foot').css('display', 'none');
-        $('.rss__sport-basket').css('display', 'none');
-    });
-
-    $('.rss__sport-subtitle-foot').trigger('click', function () {
-        // Class active
-        $(this).parent('.rss__sport-subtitles').find('.rss__sport-subtitle-active')
-            .removeClass('rss__sport-subtitle-active')
-        $(this).addClass('rss__sport-subtitle-active');
-        //
-        $('.rss__sport-foot').show;
-        $('.rss__sport-basket').hide;
-        $('.rss__sport-tennis').hide;
+        // Replace Slider
+        $('.rss__sport-slider-tennis').css('visibility', 'visible');
+        $('.rss__sport-slider-foot').css('visibility', 'hidden');
+        $('.rss__sport-slider-basket').css('visibility', 'hidden');
     });
 });
+
